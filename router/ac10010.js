436b38a77d5642a248be9f52b16e7016b19c46f6
austinwang104 <austin.wang@104.com.tw>
2019-06-18 22:24:08 +0800

    資料庫功能完成，加入API對應方法。

--- router/ac10010.js
+++ router/ac10010.js
@@ -2,8 +2,14 @@
 
 module.exports=() => {
     const router = require('express').Router();
+    
     router.post('/list', (req, res)=>{
         return res.json({"list":"data"})
     });
+
+    router.post('/get', (req, res)=>{
+    	return res.json({"get":"data"})
+    });
+
     return router;
 }
